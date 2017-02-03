b9d5045cf6dc6aa0a68f6c2be8fe104ee2afa8a0
Bingbing Huang <bing2013@cs.washington.edu>
2017-02-03 15:42:24 -0800

    update

---
+++ src/com/dropToken/Board.java
@@ -0,0 +1,14 @@
+package com.dropToken;
+
+import java.util.*;
+
+public class Board {
+
+    int rows;
+    int columns;
+
+    public Board(int rows, int colums){
+        this.rows = rows;
+        this.columns = colums;
+    }
+}
--- src/com/dropToken/Main.java
+++ src/com/dropToken/Main.java
@@ -1,8 +1,7 @@
-package com.company;
-
-import java.awt.*;
+package com.dropToken;
 
 public class Main {
+
 
     public static void main(String[] args) {
         System.out.println("Test");
