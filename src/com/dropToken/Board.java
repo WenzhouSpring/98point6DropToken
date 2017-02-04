2672788e32fea125ec238ef254d7fb44b9c6df36
“Bingbing <bing2013@uw.edu>
2017-02-03 19:02:00 -0800

    update

--- src/com/dropToken/Board.java
+++ src/com/dropToken/Board.java
@@ -4,18 +4,105 @@ import java.util.*;
 
 class Board {
 
-    private int[][] contents;
+    private int rows;
+    private int columns;
+    private int[][] payloads;
 
-    public Board(){
-        contents = new int[4][4];
+    public Board(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        this.payloads = new int[rows][columns];
     }
 
-    public void printBoard(){
+
+    public boolean insertAndCheck(int row, int column, int value) {
+        payloads[row][column] = value;
+        return checkWinning(row, column);
     }
 
+    public void printBoard() {
+        // print out the contents of the array
+        for (int i = 0; i < rows; i++) {
+            System.out.print("|");
+            for (int j = 0; j < columns; j++) {
 
-    private int convertIndex(int row, int column){
-        return
+                int valueToPrint = payloads[i][j];
+                if (valueToPrint == -1) {
+                    valueToPrint = 2;
+                }
+                System.out.print(" " + valueToPrint);
+            }
+            System.out.println();
+        }
+
+        // print out the cut-off line.
+        System.out.print("+");
+        for (int k = 0; k < columns * 2; k++) {
+            System.out.print("-");
+        }
+        System.out.println();
+
+        // print out the column number
+        for (int q = 1; q <= columns; q++) {
+            System.out.print(q + " ");
+        }
+        System.out.println();
     }
 
+    private boolean checkWinning(int row, int column) {
+
+        int score = 0;
+
+        // check the diagonal if there is possible diagonal win for the slot
+        if (((row == 0) && (column == 0)) || ((row == rows - 1) && (column == columns - 1))) {
+
+            for (int i = 0; i < rows; i++) {
+                score += payloads[i][i];
+            }
+
+            if (score == rows || score == -rows) {
+                return true;
+            }
+
+            score = 0;
+
+        }
+
+        if (((row == 0) && (column == columns - 1)) || ((row == rows - 1) && (column == 0))) {
+
+            for (int j = 0; j < rows; j++) {
+                score += payloads[j][columns - j - 1];
+            }
+
+            if (score == rows || score == -rows) {
+                return true;
+            }
+
+            score = 0;
+
+        }
+
+        // check the column
+        for (int i = 0; i < rows; i++) {
+            score += payloads[i][column];
+        }
+
+        if (score == rows || score == -rows) {
+            return true;
+        }
+
+        score = 0;
+
+        // check the row
+        for (int i = 0; i < columns; i++) {
+            score += payloads[row][i];
+        }
+
+        if (score == rows || score == -rows) {
+            return true;
+        }
+
+        return false;
+
+    }
 }
--- src/com/dropToken/Main.java
+++ src/com/dropToken/Main.java
@@ -1,8 +1,60 @@
 package com.dropToken;
+import com.dropToken.Board;
+
+import java.util.Scanner;
+
 
 public class Main {
 
+    // The column and row size for the board
+    private static final int size = 4;
+
     public static void main(String[] args) {
-        System.out.println("Test");
+
+        System.out.println("Welcome to Drop Token Game!");
+        Scanner scanner = new Scanner(System.in);
+        Board board = new Board(size, size);
+        while(true){
+
+            String[] split = scanner.nextLine().split("\\s+");
+            if(split.length != 1 && split.length != 2){
+                System.out.println("Input is not valid! Please enter a valid command!");
+                continue;
+            }
+
+            switch (split[0]){
+                case "GET":
+                    break;
+                case "BOARD":
+                    break;
+                case "PUT":
+                            try{
+                                int column = Integer.valueOf(split[1]);
+                                if(column < 1 || column > size){
+                                    throw new Exception();
+                                }
+                            }catch (Exception e){
+                                System.out.println("Input is not valid! Please enter a valid command!");
+                                continue;
+                            }
+                    break;
+                case "EXIT":
+                    break;
+                default:
+                    System.out.println("Input is not valid! Please enter a valid command!");
+                    continue;
+            }
+
+
+
+
+
+        }
+
     }
+
+
+
+
+
 }
